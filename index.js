81be3db291159ffb3524caa738d8d8671b78e511
Dilshad <hmydilshadkt@gmail.com>
2024-05-24 15:49:55 +0530

    scrollTriger and scrub

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
-// gsap.to("#box", {
-//   x: 1000,
-//   duration: 2,
+// // gsap.to("#box", {
+// //   x: 1000,
+// //   duration: 2,
+// //   delay: 0.25,
+// //   rotation: 360,
+// //   repeat: -1,
+// //   yoyo: true,
+// // });
+
+// var tl = gsap.timeline();
+// tl.from("h2", {
+//   opacity: 0,
+//   y: -30,
+//   duration: 1,
 //   delay: 0.25,
-//   rotation: 360,
-//   repeat: -1,
-//   yoyo: true,
 // });
 
-var tl = gsap.timeline();
-tl.from("h2", {
+// tl.from("li", {
+//   y: -50,
+//   opacity: 0,
+//   stagger: 0.3,
+//   duration: 1,
+// });
+// tl.from("h1", {
+//   y: 100,
+//   opacity: 0,
+//   scale: 2,
+// });
+
+gsap.from("#page1 #box", {
   opacity: 0,
-  y: -30,
-  duration: 1,
+  rotate: 360,
   delay: 0.25,
+  duration: 1,
+});
+gsap.from("h1", {
+  x: 300,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "h1",
+    scroller: "body",
+    markers: true,
+    start: "top 300px",
+  },
 });
 
-tl.from("li", {
-  y: -50,
-  opacity: 0,
-  stagger: 0.3,
-  duration: 1,
-});
-tl.from("h1", {
-  y: 100,
-  opacity: 0,
-  scale: 2,
+gsap.from("#page3 #box", {
+  scale: 0,
+  rotate: 360,
+  scrollTrigger: {
+    trigger: "#page3 #box",
+    scroller: "body",
+    markers: true,
+    start: "top 70%",
+    end: "top 30%",
+    scrub: 2,
+    pin: true,
+  },
 });
