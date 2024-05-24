b8cae9f53d64be6afa39862e472ea46f697849b1
Dilshad <hmydilshadkt@gmail.com>
2024-05-24 14:45:17 +0530

    stagger

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-gsap.to("#box", {
-  x: 1000,
-  duration: 2,
+// gsap.to("#box", {
+//   x: 1000,
+//   duration: 2,
+//   delay: 0.25,
+//   rotation: 360,
+//   repeat: -1,
+//   yoyo: true,
+// });
+
+var tl = gsap.timeline();
+tl.from("h2", {
+  opacity: 0,
+  y: -30,
+  duration: 1,
   delay: 0.25,
-  rotation: 360,
-  repeat: -1,
-  yoyo: true,
+});
+
+tl.from("li", {
+  y: -50,
+  opacity: 0,
+  stagger: 0.3,
+  duration: 1,
+});
+tl.from("h1", {
+  y: 100,
+  opacity: 0,
+  scale: 2,
 });
